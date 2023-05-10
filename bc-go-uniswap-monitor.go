4357d21d656e96fff71d6462dfc14567a1c49669
QuenumGerald <gerald.quenum@davensi.com>
2023-05-10 18:20:37 +0200

    query every minutes

--- bc-go-uniswap-monitor.go
+++ bc-go-uniswap-monitor.go
@@ -1,96 +1,68 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"time"
+
+	"github.com/levigross/grequests"
+	"github.com/tidwall/gjson"
 )
 
-type GraphQLRequest struct {
-	Query string `json:"query"`
-}
-
-type GraphQLResponse struct {
-	Data   map[string]interface{} `json:"data"`
-	Errors []struct {
-		Message string `json:"message"`
-	} `json:"errors"`
-}
+const uniswapURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
+const pairID = "0xd3d2e2692501a5c9ca623199d38826e513033a17" // Replace with your pair ID
 
 func main() {
-	// Replace with the appropriate Uniswap subgraph URL (v2 or v3)
-	uniswapURL := "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
-
-	// Replace with the specific pair's ID
-	pairID := "0x00004ee988665cdda9a1080d5792cecd16dc1220"
-
-	query := fmt.Sprintf(`
-		{
-			pair(id: "%s") {
-				id
-				token0 {
-					symbol
-				}
-				token1 {
-					symbol
-				}
-				reserve0
-				reserve1
-				volumeToken0
-				volumeToken1
-			}
+	for {
+		err := fetchData()
+		if err != nil {
+			fmt.Printf("Error fetching data: %v\n", err)
 		}
-	`, pairID)
-
-	request := GraphQLRequest{Query: query}
-	response, err := makeGraphQLRequest(uniswapURL, request)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		time.Sleep(1 * time.Minute)
 	}
-
-	if len(response.Errors) > 0 {
-		fmt.Println("GraphQL errors:", response.Errors)
-		return
-	}
-
-	pairData := response.Data["pair"].(map[string]interface{})
-	token0Symbol := pairData["token0"].(map[string]interface{})["symbol"].(string)
-	token1Symbol := pairData["token1"].(map[string]interface{})["symbol"].(string)
-	liquidityToken0 := pairData["reserve0"].(string)
-	liquidityToken1 := pairData["reserve1"].(string)
-	volumeToken0 := pairData["volumeToken0"].(string)
-	volumeToken1 := pairData["volumeToken1"].(string)
-
-	fmt.Printf("Pair: %s-%s\n", token0Symbol, token1Symbol)
-	fmt.Printf("Liquidity: %s %s, %s %s\n", liquidityToken0, token0Symbol, liquidityToken1, token1Symbol)
-	fmt.Printf("Trading Volume: %s %s, %s %s\n", volumeToken0, token0Symbol, volumeToken1, token1Symbol)
 }
 
-func makeGraphQLRequest(url string, request GraphQLRequest) (*GraphQLResponse, error) {
-	requestBody, err := json.Marshal(request)
+func fetchData() error {
+	query := `
+	{
+		pair(id: "` + pairID + `") {
+			id
+			token0 {
+				symbol
+			}
+			token1 {
+				symbol
+			}
+			reserve0
+			reserve1
+			volumeToken0
+			volumeToken1
+			volumeUSD
+		}
+	}
+	`
+
+	response, err := grequests.Post(uniswapURL, &grequests.RequestOptions{
+		JSON:    map[string]string{"query": query},
+		Headers: map[string]string{"Content-Type": "application/json"},
+	})
+	
+
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+	data := gjson.GetBytes(response.Bytes(), "data.pair")
+	token0 := data.Get("token0.symbol").String()
+	token1 := data.Get("token1.symbol").String()
+	reserve0 := data.Get("reserve0").Float()
+	reserve1 := data.Get("reserve1").Float()
+	volumeToken0 := data.Get("volumeToken0").Float()
+	volumeToken1 := data.Get("volumeToken1").Float()
+	volumeUSD := data.Get("volumeUSD").Float()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
+	fmt.Printf("Pair: %s-%s\n", token0, token1)
+	fmt.Printf("Reserves: %f %s, %f %s\n", reserve0, token0, reserve1, token1)
+	fmt.Printf("Trading Volume: %f %s, %f %s, %f USD\n", volumeToken0, token0, volumeToken1, token1, volumeUSD)
 
-	var response GraphQLResponse
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		return nil, err
-	}
-
-	return &response, nil
+	return nil
 }
